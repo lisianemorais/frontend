61eaf795ea8482f527173d4411c954a7ece0392e
Lisiane <lisi.rmorais@gmail.com>
2018-10-28 22:57:02 -0300

    ajustes envios de email

---
+++ controller/sendEmail.php
@@ -0,0 +1,30 @@
+<?php 
+require '../vendor/phpmailer/phpmailer/PHPMailerAutoload.php';
+require '../vendor/phpmailer/phpmailer/class.phpmailer.php';
+
+$dados = $_POST['data'];
+$mail = new PHPMailer();
+$mail->isSMTP();
+$mail->Host = 'smtp.gmail.com';
+$mail->SMTPAuth = true;
+$mail->SMTPSecure = 'tls';
+$mail->Username = 'limaesouzaadvogadospoa@gmail.com';
+$mail->Password = '!@12qwaszx';
+$mail->Port = 587;
+
+$mail->setFrom($dados['contact-email']);
+// $mail->addAddress($dados['contact-email'], $dados['contact-name']);
+// $mail->addAddress('limaesouzaadvogadospoa@gmail.com');
+
+$mail->isHTML(true);
+$mail->Subject = 'Lima e Souza Advogados';
+$mail->Body    = $dados['contact-message'];
+$mail->AltBody = 'Para visualizar essa mensagem acesse http://site.com.br/mail';
+
+
+if(!$mail->send()) {
+    echo 'Não foi possível enviar a mensagem.<br>';
+    echo 'Erro: ' . $mail->ErrorInfo;
+} else {
+    echo 'Mensagem enviada.';
+}
